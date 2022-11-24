601437e83d6a55731f5b6839e19a4fe906fa43dd
adriano.souza <adriano.souza@zpesystems.com>
2022-11-24 20:52:42 -0300

    feat(users): Refactoring endpoint users

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
 import express from 'express';
+import uuid, { v4 } from 'uuid';
 
 const app = express();
 
 app.use(express.json());
 
 interface UserPorps {
-  id: number;
+  id?: string;
   name: string;
   age: number;
 }
 
-const users: UserPorps[] = [
-  {id: 1, name: 'Adriano', age: 47},
-  {id: 2, name: 'Cristian', age: 14},
-  {id: 3, name: 'Grace', age: 18}
-]
+const users: UserPorps[] = []
 
 
 app.get("/users", (request, response) => {
@@ -22,32 +19,44 @@ app.get("/users", (request, response) => {
 })
 
 app.post("/users", (request, response) => {
-  const body = request.body
+  const {name, age} = request.body
 
-  console.log(body)
+  users.push({
+    id: v4(),
+    name,
+    age
+  })
 
-
-  return response.json(body);
+  return response.json(users);
 })
 
 app.put("/users/:id", (request, response) => {
   const { id } = request.params;
+  const {name, age } = request.body;
 
-  console.log(id)
+  const user = users.find(user => user.id === id);
 
-  const body = request.body;
+  if (!user) {
+    return response.status(400).json({error: "User does not exists!"})
+  }
 
-  console.log(body)
-
+  user.name = name;
+  user.age = age;
   
-  return response.json(body);
+  return response.json(user);
 })
 
 app.delete("/users/:id", (request, response) => {
   const { id } = request.params;
-  console.log(id)
+
+  const user = users.find(user => user.id === id);
+
+  users.splice(user, 1)
+  
   return response.json(id);
 })
+
+
 
 
 
